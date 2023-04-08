4d10247172769cafa8b9b78e010a43905f62e0a7
MayAlcione <myalcione@gmail.com>
2023-04-08 15:45:41 -0400

    Estructuración de index.js

--- index.js
+++ index.js
@@ -12,36 +12,45 @@ program
   .option('-v, --validate', 'Valida los enlaces encontrados')
   .option('-s, --stats', 'Muestra estadísticas de los enlaces encontrados')
   .option('-r, --recursive', 'Busca enlaces de manera recursiva en los directorios')
-  .action(async (path, options) => {
-    const { validate, stats, recursive } = options;
-    let links;
-    try {
-      links = await mdLinks(path, { validate, recursive });
-    } catch (err) {
-      console.error(`Error al leer el archivo ${path}: ${err.message}`);
-      return;
-    }
-    if (stats) {
-      let statsData;
-      try {
-        statsData = mdLinks.stats(links, path);
-      } catch (err) {
-        console.error(`Error al calcular las estadísticas: ${err.message}`);
-        return;
-      }
-      console.log(statsData);
-    } else if (validate) {
-      try {
-        const validateData = await mdLinks.validate(links);
-        console.log(validateData);
-      } catch (err) {
-        console.error(`Error al validar los enlaces: ${err.message}`);
-        return;
-      }
-    } else {
-      console.log(links);
-    }
+  .action((path, options) => {
+    return new Promise((resolve, reject) => {
+      const { validate, stats, recursive } = options;
+      let links;
+      mdLinks(path, { validate, recursive })
+        .then((links) => {
+          if (stats) {
+            let statsData;
+            try {
+              statsData = mdLinks.stats(links, path);
+            } catch (err) {
+              console.error(`Error al calcular las estadísticas: ${err.message}`);
+              reject(err);
+              return;
+            }
+            console.log(statsData);
+            resolve();
+          } else if (validate) {
+            mdLinks.validate(links)
+              .then((validateData) => {
+                console.log(validateData);
+                resolve();
+              })
+              .catch((err) => {
+                console.error(`Error al validar los enlaces: ${err.message}`);
+                reject(err);
+              });
+          } else {
+            console.log(links);
+            resolve();
+          }
+        })
+        .catch((err) => {
+          console.error(`Error al leer el archivo ${path}: ${err.message}`);
+          reject(err);
+        });
+    });
   });
+  
   module.exports = () => {
     // ...
   };
