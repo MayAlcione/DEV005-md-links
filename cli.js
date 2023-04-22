dae16eef0c79dae0a800718af7e8d0b94a15b5b4
MayAlcione <myalcione@gmail.com>
2023-04-22 16:42:56 -0400

    Cli creado

---
+++ cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const { obtenerEstadisticas } = require('./functions.js');
+const { mdlinks }= require('./index.js');
+
+const path = process.argv[2];
+const options = process.argv;
+const validate = options.includes("--validate") ? true : false;
+const stats = options.includes("--stats") ? true : false;
+
+mdlinks(path, { validate: validate})
+  .then((response) => {
+    if (validate && stats) {
+     console.log(obtenerEstadisticas(response, true));    
+  } else if (stats) {
+     console.log(obtenerEstadisticas(response));    
+  } else {
+      return console.log(response);
+    }
+  })
+  .catch((err) => console.log(err));
+  
